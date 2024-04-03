713cc06e8c36a5177c7f7b16cc51ccf206867902
SamuelPDS <duartes607@gmail.com>
2024-04-03 10:56:27 -0300

    feat: add function to get the name to search at the backend

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BackendServiceService } from 'src/app/service/backend-service.service';
+import { Customer } from 'src/app/shared/customer';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +10,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+formGroup!: FormGroup;
+formSubmitted: boolean = false;
+nameClient!: string;
 
-  constructor() { }
+
+  constructor(private formBuild: FormBuilder, private router: Router, private route: ActivatedRoute, private backend: BackendServiceService) { }
 
   ngOnInit(): void {
+    this.createForm(new Customer())
   }
 
+
+  createForm(customer: Customer) {
+    this.formGroup = this.formBuild.group({
+      name: [customer.name, [Validators.required]]
+    })
+  }
+
+
+  onSubmit(){
+    if(this.formGroup.valid) {
+      this.goTo()
+      const getName = this.backend.getBackend(this.formGroup.value['name'])
+
+    } else {
+      this.formSubmitted = true
+
+    }
+  }
+
+  getName(e: any){
+    this.nameClient = e.target.value
+    return this.nameClient
+  }
+
+  goTo() {
+    console.log('chamou')
+    this.router.navigate(['/search'])
+  }
 }
