a2b166fc67ddf8a4349df4ce10fce4f36a9b6366
Samuel <duartes607@gmail.com>
2024-04-03 19:50:37 -0300

    feat: add the form as the default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchClientComponent } from './components/search-client/search-client.component';
+import { FormComponent } from './components/form/form.component';
 
 const routes: Routes = [
-  {path:'', redirectTo: '/header', pathMatch: 'full'},
-  {path: 'header', component: HeaderComponent},
+  {path:'', redirectTo: '/form', pathMatch: 'full'},
+  {path: 'form', component: FormComponent},
   {path: 'search', pathMatch: 'full', component: SearchClientComponent},
-  
+
 ];
 
 @NgModule({
