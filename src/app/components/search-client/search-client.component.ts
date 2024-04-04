0b4cbd430a46e588b162bc89325165792d0ce174
Samuel <duartes607@gmail.com>
2024-04-03 21:15:19 -0300

    feat: add the method to get the client

--- src/app/components/search-client/search-client.component.ts
+++ src/app/components/search-client/search-client.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { BackendServiceService } from 'src/app/service/backend-service.service';
 import { HeaderComponent } from '../header/header.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Customer } from 'src/app/shared/customer';
 
 @Component({
   selector: 'app-search-client',
@@ -10,12 +12,30 @@ import { HeaderComponent } from '../header/header.component';
   providers: [HeaderComponent]
 })
 export class SearchClientComponent implements OnInit {
+  formGroup!: FormGroup;
+  formSubmitted: boolean = false;
+  nameClient!: string;
+  clientData!: any;
 
-  constructor(private backend: BackendServiceService, httpCLient: HttpClient, private cpf: HeaderComponent) { }
+  constructor(private formBuilder: FormBuilder, private backend: BackendServiceService, httpCLient: HttpClient, private cpf: HeaderComponent) { }
 
   ngOnInit(): void {
- 
+    this.createForm(new Customer());
   }
+
+  createForm(customer: Customer) {
+    this.formGroup = this.formBuilder.group({
+      name: [customer.name]
+    })
+
+  };
+
+  onSubmit(){
+    this.backend.getBackend(this.formGroup.value['name']).subscribe(res => {
+        this.clientData = res;
+      })
+
+  };
 
   // getClient(){
   //   const getCpf = `${this.backend}/${this.cpf.cpfClient}`
